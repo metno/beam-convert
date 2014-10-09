0c066f0a1dc4058b3e564b8e937fcaac69571c2e
vagrant <vagrant@ubuntu-14.04-amd64-vbox>
2014-10-09 13:09:14 +0000

    [jenkins] playing around

--- jenkins_behave.sh
+++ jenkins_behave.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-export BEAM_HOME=$HOME/beam-5.0
-export PYTHONPATH=$BEAM_HOME/modules/beam-python-5.0/beampy:$PYTHONPATH
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle
-export JDK_HOME=$JAVA_HOME
-export LD_LIBRARY_PATH=$JDK_HOME/jre/lib/amd64/server:$LD_LIBRARY_PATH
+BEAM_HOME=$HOME/beam-5.0 PYTHONPATH=$BEAM_HOME/modules/beam-python-5.0/beampy:$PYTHONPATH JAVA_HOME=/usr/lib/jvm/java-7-oracle JDK_HOME=$JAVA_HOME LD_LIBRARY_PATH=$JDK_HOME/jre/lib/amd64/server:$LD_LIBRARY_PATH behave
 behave
